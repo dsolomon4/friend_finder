d922a53e9b757552d7ecf4b84a72c92bf2ae48dc
Desiree Solomon <d.d.solomon4@gmail.com>
2018-11-17 11:43:38 -0500

    update git

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,26 @@
-// 3. Your `htmlRoutes.js` file should include two routes:
+var path = require("path");
+
+module.exports = function(app) {
+
+// =============================================================
+
+
+
 
 //    * A GET Route to `/survey` which should display the survey page.
 app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"))
-})
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
 
 // A default, catch-all route that leads to `home.html` which displays the home page. 
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"))
-})
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+//error
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+}
--- server.js
+++ server.js
@@ -16,3 +16,8 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 // =============================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
