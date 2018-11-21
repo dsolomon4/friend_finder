097942af52ab57af6bd2d3fa2167497ea1631cf2
Desiree Solomon <d.d.solomon4@gmail.com>
2018-11-20 21:21:01 -0500

    update gitignore

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,5 @@
-var friendsObject =
+
+var friends = [
 {
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -14,4 +15,7 @@ var friendsObject =
         4,
         1
       ]
-  }
+  }]
+
+  
+module.exports = friends;
--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(express.json());
 
 require("./app/routing/htmlRoutes")(app);
 
+require("./app/routing/apiRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
