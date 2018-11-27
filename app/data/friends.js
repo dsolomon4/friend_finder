bcead80ffe5ce1201f23c1badd4885a83704fe75
Desiree Solomon <d.d.solomon4@gmail.com>
2018-11-27 17:58:22 -0500

    updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,15 +5,60 @@ var friends = [
     "photo":"https://www.biography.com/.image/t_share/MTE1ODA0OTcxODg1ODIzNTAx/betty-white-9542614-1-402.jpg",
     "scores":[
         5,
-        1,
-        4,
-        4,
         5,
-        1,
-        2,
         5,
-        4,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+      ]
+  },{
+    "name":"Justin Timberlake",
+    "photo":"https://s3.r29static.com//bin/entry/4c5/x,80/1705671/image.jpg",
+    "scores":[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
         1
+      ]
+  },{
+    "name":"Beyonce",
+    "photo":"https://images-na.ssl-images-amazon.com/images/I/A15dQM39ELL._CR0,0,3840,2880_._SL1000_.jpg",
+    "scores":[
+        4,
+        4,
+        4,
+        4,
+        4,
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  },{
+    "name":"Leonardo Dicaprio",
+    "photo":"http://specials-images.forbesimg.com/imageserve/558c0172e4b0425fd034f8ba/280x425.jpg?fit=scale&background=000000",
+    "scores":[
+        3,
+        3,
+        3,
+        3,
+        3,
+        2,
+        2,
+        2,
+        2,
+        2
       ]
   }]
 
